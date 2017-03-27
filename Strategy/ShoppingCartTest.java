958de9647a6f2ffb5ef240e2d809f8b90173a3a3
vetriselvan1187 <vetriblogger1187@gmail.com>
2017-03-26 16:47:22 -1100

    Strategy added

---
+++ Strategy/ShoppingCartTest.java
@@ -0,0 +1,16 @@
+
+public class ShoppingCartTest {
+
+	public static void main(String[] args) {
+		ShoppingCart cart = new ShoppingCart();
+		Item item1 = new Item("1234", 10);
+		Item item2 = new Item("5678", 40);
+
+		cart.addItem(item1);
+		cart.addItem(item2);
+
+		cart.pay(new PaypalStrategy("vetriblogger1187@gmail.com","password"));
+		cart.pay(new CreditCardStrategy("Vetriselvan", "2223444232223", "786", "21/24"));
+
+	}
+}
