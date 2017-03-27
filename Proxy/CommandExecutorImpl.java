8248cb4a83d714abfc618eeffc04dfa568065fb4
vetriselvan1187 <vetriblogger1187@gmail.com>
2017-03-26 15:30:08 -1100

    Proxy added

---
+++ Proxy/CommandExecutorImpl.java
@@ -0,0 +1,11 @@
+
+import java.io.IOException;
+
+public class CommandExecutorImpl implements CommandExecutor {
+
+	@Override
+	public void runCommand(String cmd) throws IOException {
+		Runtime.getRuntime().exec(cmd);
+		System.out.println("'"+cmd+" ' command executed");
+	}
+}
